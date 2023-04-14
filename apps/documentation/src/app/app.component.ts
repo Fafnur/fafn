8b28482aba39eaa74d71b9dfe172d68772f5ab29
Aleksandr Serenko <fafnur@yandex.ru>
2023-04-14 22:14:48 +0700

    [docs] Adding sample

--- apps/documentation/src/app/app.component.ts
+++ apps/documentation/src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ContainerComponent, GRID } from '@fafn/components';
 import { LocalDBService } from '@fafn/core';
 
 const TOKENS = 'tokens';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ContainerComponent, GRID],
   selector: 'fafn-root',
-  template: '<router-outlet></router-outlet>',
+  template: `<router-outlet></router-outlet>
+    <fafn-container>
+      <fafn-row tablet="">
+        <fafn-column tablet="6" web="3">1</fafn-column>
+        <fafn-column tablet="6" web="3">2</fafn-column>
+        <fafn-column tablet="6" web="3">3</fafn-column>
+        <fafn-column tablet="6" web="3">4</fafn-column>
+      </fafn-row>
+    </fafn-container>`,
 })
 export class AppComponent implements OnInit {
   constructor(private readonly localDBService: LocalDBService<typeof TOKENS | 'books'>) {}
