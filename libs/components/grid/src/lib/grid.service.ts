b248de4e48c3b70c152bbd6399e45e540a70ca77
Aleksandr Serenko <fafnur@yandex.ru>
2023-04-19 13:46:12 +0700

    [components][grid] Adding override breakpoints

--- libs/components/grid/src/lib/grid.service.ts
+++ libs/components/grid/src/lib/grid.service.ts
@@ -1,36 +1,61 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, InjectionToken, Optional } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
-export enum GridMode {
+export const BREAKPOINTS = new InjectionToken<typeof Breakpoints>('Breakpoints');
+
+export const enum BreakpointType {
+  XSmall = 'xsmall',
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+  XLarge = 'xlarge',
+
   Handset = 'handset',
   Tablet = 'tablet',
   Web = 'web',
-}
 
-export const LAYOUT_SHORT_TYPES_MAP = {
-  [Breakpoints.Handset]: Breakpoints.Handset,
-  [Breakpoints.HandsetPortrait]: Breakpoints.HandsetPortrait,
-  [Breakpoints.HandsetLandscape]: Breakpoints.HandsetLandscape,
-  [Breakpoints.Tablet]: Breakpoints.Tablet,
-  [Breakpoints.TabletPortrait]: Breakpoints.TabletPortrait,
-  [Breakpoints.TabletLandscape]: Breakpoints.TabletLandscape,
-  [Breakpoints.Web]: Breakpoints.Web,
-  [Breakpoints.WebPortrait]: Breakpoints.WebPortrait,
-  [Breakpoints.WebLandscape]: Breakpoints.WebLandscape,
-  [GridMode.Handset]: Breakpoints.Handset,
-  [GridMode.Tablet]: Breakpoints.Tablet,
-  [GridMode.Web]: Breakpoints.Web,
-};
+  HandsetPortrait = 'handsetPortrait',
+  TabletPortrait = 'tabletPortrait',
+  WebPortrait = 'webPortrait',
+  HandsetLandscape = 'handsetLandscape',
+  TabletLandscape = 'tabletLandscape',
+  WebLandscape = 'webLandscape',
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class GridService {
-  constructor(private readonly breakpointObserver: BreakpointObserver) {}
+  private readonly breakpoints: typeof Breakpoints;
+  private readonly map: Record<BreakpointType, string>;
 
-  up(types: string | string[]): Observable<boolean> {
-    const observeTypes = Array.isArray(types) ? types.map((type) => LAYOUT_SHORT_TYPES_MAP[type]) : LAYOUT_SHORT_TYPES_MAP[types];
+  constructor(
+    private readonly breakpointObserver: BreakpointObserver,
+    @Optional() @Inject(BREAKPOINTS) breakpoints: typeof Breakpoints | null
+  ) {
+    this.breakpoints = breakpoints ?? Breakpoints;
+
+    this.map = {
+      [BreakpointType.Handset]: this.breakpoints.Handset,
+      [BreakpointType.HandsetPortrait]: this.breakpoints.HandsetPortrait,
+      [BreakpointType.HandsetLandscape]: this.breakpoints.HandsetLandscape,
+      [BreakpointType.Tablet]: this.breakpoints.Tablet,
+      [BreakpointType.TabletPortrait]: this.breakpoints.TabletPortrait,
+      [BreakpointType.TabletLandscape]: this.breakpoints.TabletLandscape,
+      [BreakpointType.Web]: this.breakpoints.Web,
+      [BreakpointType.WebPortrait]: this.breakpoints.WebPortrait,
+      [BreakpointType.WebLandscape]: this.breakpoints.WebLandscape,
+      [BreakpointType.XSmall]: this.breakpoints.XSmall,
+      [BreakpointType.Small]: this.breakpoints.Small,
+      [BreakpointType.Medium]: this.breakpoints.Medium,
+      [BreakpointType.Large]: this.breakpoints.Large,
+      [BreakpointType.XLarge]: this.breakpoints.XLarge,
+    };
+  }
+
+  up(types: BreakpointType | BreakpointType[]): Observable<boolean> {
+    const observeTypes = Array.isArray(types) ? types.map((type) => this.map[type]) : this.map[types];
 
     return this.breakpointObserver.observe(observeTypes).pipe(map((breakpoints) => breakpoints.matches));
   }
