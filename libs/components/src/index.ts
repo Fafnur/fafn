62c901b94fbd6fca52df19afe611c8ec6a24d247
Aleksandr Serenko <fafnur@yandex.ru>
2023-04-24 16:30:58 +0700

    [components][icons] Generating icons lib

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -1,5 +1,5 @@
 // Need for build
+export * from './lib/common-behaviors/color';
 export * from './lib/common-behaviors/constructor';
 export * from './lib/common-behaviors/disabled';
-export * from './lib/common-behaviors/color';
 export * from './lib/common-behaviors/size';
