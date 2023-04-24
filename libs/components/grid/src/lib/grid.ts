6e716ef4e8cc5b8b18da0aca580fa5826b2b711b
Aleksandr Serenko <fafnur@yandex.ru>
2023-04-24 16:35:01 +0700

    [components][grid] Change grid structure

--- libs/components/grid/src/lib/grid.ts
+++ libs/components/grid/src/lib/grid.ts
@@ -1,7 +1,7 @@
-import { ColumnComponent } from './components/column/column.component';
-import { RowComponent } from './components/row/row.component';
-import { HandsetDirective } from './directives/handset.directive';
-import { TabletDirective } from './directives/tablet.directive';
-import { WebDirective } from './directives/web.directive';
+import { ColumnComponent } from './column/column.component';
+import { HandsetDirective } from './column/handset.directive';
+import { TabletDirective } from './column/tablet.directive';
+import { WebDirective } from './column/web.directive';
+import { RowComponent } from './row/row.component';
 
 export const GRID = [RowComponent, ColumnComponent, HandsetDirective, TabletDirective, WebDirective];
