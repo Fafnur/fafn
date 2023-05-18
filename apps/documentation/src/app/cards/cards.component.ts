02d06dc418b3b7962d5275debfdcaf055522885c
Aleksandr Serenko <fafnur@yandex.ru>
2023-05-18 11:47:09 +0700

    [components] Adding part styles for card actions

--- apps/documentation/src/app/cards/cards.component.ts
+++ apps/documentation/src/app/cards/cards.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { FafnBody } from '@fafn/components/body';
-import { FafnCard, FafnCardContent } from '@fafn/components/cards';
+import { FafnButton } from '@fafn/components/buttons';
+import { FafnCard, FafnCardActions, FafnCardContent } from '@fafn/components/cards';
 import { FafnContainer } from '@fafn/components/container';
 import { FafnColumn, FafnRow, FafnTablet } from '@fafn/components/grid';
 import { FafnHeadline } from '@fafn/components/headline';
@@ -14,6 +15,19 @@ import { FafnTitle } from '@fafn/components/title';
   styleUrls: ['./cards.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [FafnCard, FafnContainer, FafnImage, FafnRow, FafnColumn, FafnTablet, FafnHeadline, FafnTitle, FafnCardContent, FafnBody],
+  imports: [
+    FafnCard,
+    FafnContainer,
+    FafnImage,
+    FafnRow,
+    FafnColumn,
+    FafnTablet,
+    FafnHeadline,
+    FafnTitle,
+    FafnCardContent,
+    FafnBody,
+    FafnCardActions,
+    FafnButton,
+  ],
 })
 export class CardsComponent {}
