cdc626b01cf823f428a58b7fd64fa2941828330a
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-30 14:35:11 +0700

    [components][tag] Changing styles for tag

--- libs/components/tag/src/lib/tag.component.ts
+++ libs/components/tag/src/lib/tag.component.ts
@@ -1,10 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { SizeDirective } from '@fafn/components';
+
 @Component({
   selector: 'fafn-tag,[fafnTag]',
   template: '<ng-content></ng-content>',
   styleUrls: ['./tag.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  hostDirectives: [
+    {
+      directive: SizeDirective,
+      inputs: ['size'],
+    },
+  ],
 })
 export class TagComponent {}
