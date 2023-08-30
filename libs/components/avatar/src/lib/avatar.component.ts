c2052ab6d5a935c661a744f212d4139b5e242f9f
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-30 19:38:04 +0700

    [components][avatar] Changing styles for avatar

--- libs/components/avatar/src/lib/avatar.component.ts
+++ libs/components/avatar/src/lib/avatar.component.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'fafn-avatar,[fafnAvatar]',
+  selector: 'fafn-avatar',
   template: '<ng-content></ng-content>',
   styleUrls: ['./avatar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+  host: {
+    class: 'fafn-avatar',
+  },
 })
 export class AvatarComponent {}
