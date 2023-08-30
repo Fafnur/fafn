5dafa52ea3119096c6b9e95f0f4d27da9a67d365
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-30 16:25:31 +0700

    [components][hint] Changing styles for hint

--- libs/components/hint/src/lib/hint.component.ts
+++ libs/components/hint/src/lib/hint.component.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'fafn-hint,[fafnHint]',
+  selector: 'fafn-hint',
   template: '<ng-content></ng-content>',
   styleUrls: ['./hint.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+  host: {
+    class: 'fafn-hint',
+  },
 })
 export class HintComponent {}
