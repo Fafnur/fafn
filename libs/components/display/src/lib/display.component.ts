1c61128ad94d0908b835b89f45a6ce5cf3c5b3f2
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-30 18:38:56 +0700

    [components][display] Changing styles for display

--- libs/components/display/src/lib/display.component.ts
+++ libs/components/display/src/lib/display.component.ts
@@ -1,10 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { SizeDirective } from '@fafn/components';
+
 @Component({
-  selector: 'fafn-display,[fafnDisplay]',
+  selector: 'fafn-display',
   template: '<ng-content></ng-content>',
   styleUrls: ['./display.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+  host: {
+    class: 'fafn-display',
+  },
+  hostDirectives: [
+    {
+      directive: SizeDirective,
+      inputs: ['size'],
+    },
+  ],
 })
 export class DisplayComponent {}
