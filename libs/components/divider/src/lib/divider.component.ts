ffc8ff9fc862458ca590ee619bcd0d1145833d13
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-30 18:37:30 +0700

    [components][divider] Changing styles for divider

--- libs/components/divider/src/lib/divider.component.ts
+++ libs/components/divider/src/lib/divider.component.ts
@@ -1,10 +1,27 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { InsetDirective } from './inset.directive';
+import { MiddleInsetDirective } from './middle-inset.directive';
+
 @Component({
-  selector: 'fafn-divider,[fafnDivider]',
+  selector: 'fafn-divider',
   template: '<ng-content></ng-content>',
   styleUrls: ['./divider.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+  host: {
+    class: 'fafn-divider',
+  },
+  hostDirectives: [
+    {
+      directive: InsetDirective,
+      inputs: ['inset'],
+    },
+    {
+      directive: MiddleInsetDirective,
+      inputs: ['middle-inset'],
+    },
+  ],
 })
 export class DividerComponent {}
--- libs/components/divider/src/lib/middle-inset.directive.ts
+++ libs/components/divider/src/lib/middle-inset.directive.ts
@@ -1,7 +1,15 @@
-import { Directive } from '@angular/core';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
-  selector: 'fafn-divider[middle-inset],[fafnDivider][fafnMiddleInset]',
+  // eslint-disable-next-line @angular-eslint/directive-selector
+  selector: 'fafn-divider[middle-inset]',
   standalone: true,
 })
-export class MiddleInsetDirective {}
+export class MiddleInsetDirective {
+  @Input('middle-inset') middleInset: boolean | string | undefined | null;
+
+  @HostBinding('class.fafn-middle-inset') get isMiddleInset(): boolean {
+    return coerceBooleanProperty(this.middleInset);
+  }
+}
